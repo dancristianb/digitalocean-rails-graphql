f702abfb1cc52008d493ade29e570468b6227227
Dan Bota <dan.cristian.bota@gmail.com>
2021-02-07 20:11:01 +0200

    remove extra generator fields

--- app/graphql/types/note_type.rb
+++ app/graphql/types/note_type.rb
@@ -1,8 +1,5 @@
 module Types
   class NoteType < Types::BaseObject
-    field :id, ID, null: false
-    field :title, String, null: true
-    field :body, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :id, ID, null: false
